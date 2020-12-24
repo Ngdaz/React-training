67e4250f9a3f3e7fd04a3170b79698c689232552
portgasdax99 <ngdaaz@gmail.com>
2020-12-24 09:58:44 +0700

    add header ( icon, link)

--- src/App.js
+++ src/App.js
@@ -7,11 +7,13 @@ import { BrowserRouter, Route, Switch } from 'react-router-dom';
 
 import HomePage from './page/components/homepage.component'
 import ShopPage from  './page/shop/shope.component'
+import Header from './components/header/header.component'
 
 
 function App() {
   return (
     <div>
+      <Header />
       <Switch>
         <Route exact path='/' component={HomePage}/>
         <Route path='/shop' component={ShopPage}/>
