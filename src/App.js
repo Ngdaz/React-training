ed895926582595c2ecb7d67b481e89e7e3e73e6d
portgasdax99 <ngdaaz@gmail.com>
2021-01-02 11:44:07 +0700

    add redux and implimented userReducer and user action to header and app components

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React from 'react'
 import './App.css';
 
-import { Route, Switch } from 'react-router-dom';
+import { Route, Switch, Redirect } from 'react-router-dom';
 
 
 
@@ -15,6 +15,8 @@ import { setCurrentUser } from './redux/user/user.action'
 
 
 class App extends React.Component {
+
+  setCurrentUser = this.props 
 
   unsubcribeFromAuth = null;
 
@@ -31,11 +33,10 @@ class App extends React.Component {
               id: snapShot.id,
               ...snapShot.data()
             })
-          // console.log(this.state)
         }) 
         
       }else{
-        setCurrentUser(userAuth)
+          setCurrentUser(userAuth)
       }
       
     })
@@ -53,15 +54,23 @@ class App extends React.Component {
           <Switch>
             <Route exact path='/' component={HomePage}/>
             <Route path='/shop' component={ShopPage}/>
-            <Route path='/signin' component={SignInAndSignUpPage}/> 
+            <Route exact path='/signin' render={() => this.props.currentUser ? (<Redirect to='./'/>) : (<SignInAndSignUpPage/>) } />
           </Switch>
         </div>
       );
   }
 }
 
+const mapStateToProps = ({ user }) => ({
+   currentUser: user.currentUser
+})
+
 const mapDispatchToProps = dispatch => ({
   setCurrentUser: user => dispatch(setCurrentUser(user))
 })
 
-export default connect(null, mapDispatchToProps) (App);
+export default connect(
+  mapStateToProps ,
+  mapDispatchToProps, 
+  
+  ) (App);
